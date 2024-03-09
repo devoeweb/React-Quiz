830c66ab9fbcc9b9c6c5c728cda0ce42ec94aba4
devoeweb <162696477+devoeweb@users.noreply.github.com>
2024-03-09 12:12:14 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./index.scss";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+];
+
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game({ question,onClickVariant }) {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: "20%" }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text,index) => (
+          <li onClick={()=> onClickVariant(index)} key={text}>{text}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function App() {
+  const [step, setStep] = React.useState(0);
+  const question = questions[step];
+  
+  const onClickVariant = (index) => {
+    console.log(step,index);
+  }
+  return (
+    <div className="App">
+      <Game question={question} onClickVariant={onClickVariant} />
+      {/* <Result /> */}
+    </div>
+  );
+}
+
+export default App;
